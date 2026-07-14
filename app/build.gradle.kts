6dc3926c3307e71ef75a775ec5db10a70f7d09fe
zacode123 <zuliabegum0@gmail.com>
2026-07-14 10:56:07 +0530

    Change applicationId to 'com.auramath.app'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.auramath.kxmpzq"
+    applicationId = "com.auramath.app"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
